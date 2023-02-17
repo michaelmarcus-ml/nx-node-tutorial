7b57cbf465b4c64706362d5e55c3409d86697155
Michael Marcus <michael.marcus@missionlane.com>
2023-02-17 16:21:59 -0500

    Generate auth library

---
+++ jest.config.app.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-node-tutorial',
+  preset: './jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/nx-node-tutorial',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+};
--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,5 @@
-/* eslint-disable */
+import { getJestProjects } from '@nrwl/jest';
+
 export default {
-  displayName: 'nx-node-tutorial',
-  preset: './jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/nx-node-tutorial',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
+  projects: getJestProjects(),
 };
